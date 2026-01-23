20733f2e96bd69695df49158360f343a15205c61
Fra-Napolitano <f.napolitano@betacom.it>
2026-01-23 11:00:15 +0100

    Come funziona Spring Boot

--- Esercitazione/src/main/java/com/esercitazione/Esercitazione/EsercitazioneApplication.java
+++ Esercitazione/src/main/java/com/esercitazione/Esercitazione/EsercitazioneApplication.java
@@ -1,10 +1,13 @@
 package com.esercitazione.Esercitazione;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.AbstractApplicationContext;
 
 @Configuration
+@ComponentScan
+// stessa cosa di scrivere solo @SpringBootApplication
 public class EsercitazioneApplication {
 
 	public static void main(String[] args) {
